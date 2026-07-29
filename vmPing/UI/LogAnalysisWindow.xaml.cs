f58ca79be15d4209c70da7584750aa60ada5afdc
lastexile2024 <dragonsolo2011@live.com>
2026-07-29 12:22:48 +0800

    remove: bar chart option - OxyPlot 2.1.2 lacks usable bar series for DateTimeAxis

--- vmPing/UI/LogAnalysisWindow.xaml.cs
+++ vmPing/UI/LogAnalysisWindow.xaml.cs
@@ -283,37 +283,7 @@ namespace vmPing.UI
 
             var points = buckets.Select(b => DateTimeAxis.CreateDataPoint(b.BucketTime, b.AvgRtt)).ToList();
 
-            if (chartType == 2)
-            {
-                var totalSpan = (maxTime - minTime).TotalDays;
-                var barWidth = totalSpan / Math.Max(buckets.Count, 1) * 0.7;
-                if (barWidth <= 0) barWidth = 0.01;
-                var halfW = barWidth / 2;
-
-                var barSeries = new AreaSeries
-                {
-                    Color = OxyColor.FromRgb(0x3b, 0x82, 0xf6),
-                    Fill = OxyColor.FromRgb(0x3b, 0x82, 0xf6),
-                    MarkerType = MarkerType.None
-                };
-
-                foreach (var b in buckets)
-                {
-                    var x = DateTimeAxis.ToDouble(b.BucketTime);
-                    var rtt = b.AvgRtt;
-                    barSeries.Points.Add(new DataPoint(x - halfW, 0));
-                    barSeries.Points.Add(new DataPoint(x - halfW, rtt));
-                    barSeries.Points.Add(new DataPoint(x + halfW, rtt));
-                    barSeries.Points.Add(new DataPoint(x + halfW, 0));
-                    barSeries.Points2.Add(new DataPoint(x - halfW, 0));
-                    barSeries.Points2.Add(new DataPoint(x - halfW, 0));
-                    barSeries.Points2.Add(new DataPoint(x + halfW, 0));
-                    barSeries.Points2.Add(new DataPoint(x + halfW, 0));
-                }
-
-                model.Series.Add(barSeries);
-            }
-            else if (chartType == 1)
+            if (chartType >= 1)
             {
                 var areaSeries = new AreaSeries
                 {
